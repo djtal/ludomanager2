7929f07c7d30e984180156c8ae60772be8bc1041
djtal <guillaume.garcera@gmail.com>
2012-01-25 22:41:56 +0100

    initial commit fro reloaded  version of ludomanager
    preliminary work on a new design line for the moment only
    use twitter-bootrap for css

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ludomanager2::Application.config.secret_token = '69c430151cc0dde1f08f936d5923b7d751ddcfaefd9982ea443a4ae1e33207f4dba9e2f5d63eaadafa76f6ec79cd8beb65fa263991a0f533bba48ab3a89fa55e'
---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Game.create([{name: 'Pandemie', min: 2, max: 5, level: 3}, {name: 'Castle Panic', min: 1, max: 6, level: 2 }])
