39f44f5d95db8b71e113f9391a1ccc2696047425
djtal <guillaume.garcera@gmail.com>
2013-08-12 23:24:34 +0200

    update carrierwave to use S3

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,12 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider               => 'AWS',                        # required
+    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],                        # required
+    :aws_secret_access_key  =>ENV['AWS_SECRET_ACCESS_KEY'],                        # required
+    :region                 => 'Ireland',                  # optional, defaults to 'us-east-1'
+    :host                   => 's3.amazonaws.com',             # optional, defaults to nil
+  }
+  config.fog_directory  = 'ludomanager'                     # required
+  config.fog_public     = false                                   # optional, defaults to true
+  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+end
