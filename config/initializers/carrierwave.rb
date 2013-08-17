af01add7f41a668294c5155b21d43a40b9b71197
djtal <guillaume.garcera@gmail.com>
2013-08-17 23:00:22 +0200

    continue slim comversion & tweak carrierwave setup
    update to latest bootstrap

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,7 +6,7 @@ CarrierWave.configure do |config|
     :region                 => 'Ireland',                  # optional, defaults to 'us-east-1'
     :host                   => 's3-external-3.amazonaws.com',             # optional, defaults to nil
   }
-  config.fog_directory  = 'ludomanager'                     # required
+  config.fog_directory  = "ludomanager-#{Rails.env}"                     # required
   config.fog_public     = false                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
