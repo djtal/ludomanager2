e60e1a5c324756aef1f88441004e3aa7b278c229
djtal <guillaume.garcera@gmail.com>
2013-08-25 00:48:39 +0200

    Update design
    * better show game card
    * better game index

--- app/assets/javascripts/behaviour.coffee
+++ app/assets/javascripts/behaviour.coffee
@@ -3,3 +3,6 @@ $ ->
   $("#editor_country_id").select2()
   $("#person_country_id").select2()
   $("#game_author_ids").select2()
+  $('body').tooltip({ selector: '[rel="tooltip"]' })
+  $('body').popover({ selector: '[rel="popover"]' })
+
--- app/helpers/editors_helper.rb
+++ app/helpers/editors_helper.rb
@@ -1,2 +1,20 @@
 module EditorsHelper
+  # Generate popover for edititor link if format is allowed and editions present
+  # *editions* : all editions of one game for one editor
+  #
+  def options_for_editor_popover(format, editor, editions, opts = {}, &block)
+    options = {
+      title: editor.name,
+      id: dom_id(editor)
+    }.merge(opts)
+    if :thumb == format && editions.any?
+      editions = (editions || []).select { |e| e.editor_id == editor.id }
+      options["rel"] = "popover"
+      options["data-trigger"] = "hover"
+      options["data-placement"] = "right"
+      options["data-html"] =  true
+      options["data-content"] = capture(editions,&block)
+    end
+    options
+  end
 end
